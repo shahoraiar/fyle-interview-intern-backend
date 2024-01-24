6e540a937ad822fe7fbe8d9740ece16032c39cf0
shahoraiar <shahoraiar2000@gmail.com>
2024-01-24 12:47:34 +0600

    run successful

--- core/server.py
+++ core/server.py
@@ -1,12 +1,16 @@
+import sys
+sys.path.append("E:\\Fyle\\fyle-interview-intern-backend")
+
 from flask import jsonify
 from marshmallow.exceptions import ValidationError
+# from fyle_interview_intern_backend import app
 from core import app
 from core.apis.assignments import student_assignments_resources, teacher_assignments_resources
 from core.libs import helpers
 from core.libs.exceptions import FyleError
 from werkzeug.exceptions import HTTPException
 
-from sqlalchemy.exc import IntegrityError
+from sqlalchemy.exc import IntegrityError 
 
 app.register_blueprint(student_assignments_resources, url_prefix='/student')
 app.register_blueprint(teacher_assignments_resources, url_prefix='/teacher')
@@ -42,3 +46,8 @@ def handle_error(err):
         ), err.code
 
     raise err
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+
